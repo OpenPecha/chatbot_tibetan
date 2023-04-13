806b298c42470f45e943b8321a07b55abf4b15c2
pechawa <openpecha-bot@openpecha.org>
2023-04-13 14:22:07 +0530

    improve readability

--- app.py
+++ app.py
@@ -1,26 +1,39 @@
+import os
+import random
+import time
+from typing import Dict
+
 import gradio as gr
 
-def add_text(history, text):
-    history = history + [(text, None)]
-    return history, ""
+API_KEY = os.getenv("OPENAI_API_KEY")
+
+ROLE_USER = "user"
+ROLE_ASSISTANT = "assistant"
+HISTROY = Dict[str, str]  # {"role": "user|assistant", "content": "text"}
+
+
+def user(user_message, history):
+    return "", history + [[user_message, None]]
 
 
 def bot(history):
-    response = "**That's cool!**"
-    history[-1][1] = response
+    bot_message = random.choice(["Yes", "No"])
+    history[-1][1] = bot_message
+    time.sleep(1)
     return history
 
+
 with gr.Blocks() as demo:
-    chatbot = gr.Chatbot([], elem_id="chatbot").style(height=750)
+    history = gr.Chatbot(label="Tibetan Chatbot").style(height=750)
 
-    with gr.Column(scale=0.85):
-        txt = gr.Textbox(
-            show_label=False,
-            placeholder="Enter text and press enter",
-        ).style(container=False)
-
-    txt.submit(add_text, [chatbot, txt], [chatbot, txt]).then(
-        bot, chatbot, chatbot
+    msg = gr.Textbox(
+        show_label=False, placeholder="Type a message here and press enter"
     )
+    msg.submit(user, [msg, history], [msg, history], queue=False).then(
+        bot, history, history
+    )
+
+    clear = gr.Button("New Chat")
+    clear.click(lambda: None, None, history, queue=False)
 
 demo.launch()
