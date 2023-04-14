2a675e46a63cdc3d7a3f3affa6153d171f21ca44
pechawa <openpecha-bot@openpecha.org>
2023-04-14 17:36:04 +0530

    feat: store user and bot msg pair in db

---
+++ store.py
@@ -0,0 +1,45 @@
+import json
+import uuid
+from typing import Tuple
+
+import boto3
+
+dynamodb = boto3.resource("dynamodb", region_name="us-east-1")
+all_chats_table = dynamodb.Table("ChatbotTibetanAllChats")
+
+
+def store_message_pair(chat_id: str, msg_pair: Tuple[str, str], lang: str, order: int):
+    """Store the chat history to DynamoDB
+
+    Args:
+        chat_id: The ID of the chat
+        msg_pair: tuple with 2 items (user_message, bot_response)
+        lang: The language of the msg_pair
+        order: The order of the msg_pair in chat history
+    """
+
+    # Add the new message to the chat history
+    msg_pair_id = uuid.uuid4().hex[:4]
+    response = all_chats_table.put_item(
+        Item={
+            "msg_pair_id": msg_pair_id,
+            "msg_pair": json.dumps(msg_pair),
+            "lang": lang,
+            "order": order,
+            "chat_id": chat_id,
+        }
+    )
+    return response
+
+
+if __name__ == "__main__":
+    # Replace with your own DynamoDB table name and chat ID
+    chat_id = uuid.uuid4().hex[:4]
+
+    # Replace with your own chat history (list of tuples or list of dictionaries)
+    chat_history = [
+        ("User", "Hello, how are you?"),
+        ("Chatbot", "I am fine, thank you!"),
+    ]
+    for i, msg_pair in enumerate(chat_history):
+        store_message_pair(chat_id, msg_pair, "en", i)
